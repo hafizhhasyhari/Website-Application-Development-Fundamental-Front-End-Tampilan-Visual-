0ef2c9b9948c7c089c1b204d2ff841803c63ed5f
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-27 00:49:06 +0700

    Create 006-Asynchronous-Latihan-1.js

---
+++ 006 Materi Asynchronous/006-Asynchronous-Latihan-1.js
@@ -0,0 +1,11 @@
+// Asynchronous Latihan 1
+// author by hafizhhasyhari
+
+console.log("selamat datang");
+
+setTimeout(() => {
+    console.log("terima kasih udah mampir,silahkan logout");
+    
+},3000)
+
+console.log("ada yang bisa dibantu,tunggu 3 detik");
