f0490b204184bbf37310181904e2e599578baf2b
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-27 00:40:37 +0700

    Create 002-latihan-2.js

---
+++ 002 Materi Destructuring (Destrukturisasi)/002-latihan-2.js
@@ -0,0 +1,37 @@
+// Destructuring pada JavaScript merupakan sintaks yang dapat mengeluarkan nilai dari array atau properties dari sebuah object ke dalam satuan yang lebih kecil.
+
+// Destucturing Object
+const biodata = {
+    namaDepan: "Hafizh",
+    namaBelakang: "Asyhari",
+    umur: 21
+}
+
+const pecahkan = { satu: namaDepan, dua: namaBelakang, tiga: umur } = biodata;
+
+console.log(pecahkan);
+
+// Destructuring Array
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+const [firstFood, secondFood, thirdFood, fourthFood, herFood = "Salad"] = favorites;
+
+console.log(firstFood);
+console.log(secondFood);
+console.log(thirdFood);
+console.log(fourthFood);
+console.log(herFood);
+
+
+// Destructuring Assignment
+let a = 1;
+let b = 2;
+
+console.log("Sebelum swap");
+console.log("Nilai a: " + a);
+console.log("Nilai b: " + b);
+
+[a, b] = [b, a]
+
+console.log("Setelah swap");
+console.log("Nilai a: " + a);
+console.log("Nilai b: " + b);
